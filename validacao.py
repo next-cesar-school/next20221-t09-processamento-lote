8a40574c4baf2c7c990450c195c3802fb5898e26
Oligonautaa <Oligonauta@gmail.com>
2022-06-10 16:25:47 -0300

    fix: Ajustando o arquivo de validação de colunas

--- validacao.py
+++ validacao.py
@@ -1,20 +1,24 @@
 # Importando o pandas
 import pandas as pd
 
-# Lendo o arquivo csv
+# Lendo e exibindo o arquivo csv
 df = pd.read_csv('./csv/bancoColaboradores.csv', encoding="UTF-8")
-# Exibindo o arquivo
 print(df)
-# Definindo ordem das colunas
-col_order = ["data_fim", "id_usuario", "data_inicio", "hora_inicio", "hora_fim"]
+
+# Definindo a ordem das colunas
+
+# Proposta 1
+# col_order = ["data_fim", "id_usuario", "data_inicio", "hora_inicio", "hora_fim"]
+
+# Proposta 2
+col_order = df[0:]
 
 # Ajustando a ordem das colunas
 
-#Proposta 1
-df_validado = df.reindex(columns=col_order, copy=False)
+# Proposta 1
+# df_validado = df.reindex(columns=col_order, copy=False)
+
+# Proposta 2
+df_validado = df.reindex(columns=col_order.columns, copy=False)
+
 print(df_validado)
-
-#Proposta 2
-# df_validado = pd.DataFrame(df, columns=col_order)
-# print(df_validado)
-
