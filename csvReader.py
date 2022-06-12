478aad09227a362d70e45831757a7864251b4a62
Victor Amorim (Harry) <victorlbamorim@gmail.com>
2022-06-12 14:46:58 -0300

    add csv and csvReader

--- csvReader.py
+++ csvReader.py
@@ -1,14 +1,24 @@
 from flask import Flask
 import pandas as pd
+import sys
 
 app = Flask(__name__)
 
 
 def main():
-    df = pd.read_csv('./csv/bancoColaboradores.csv', chunksize=5)
-    for mini_df in df:
-        print(mini_df)
-        break
+    if len(sys.argv) < 2:
+        print('Você não inseriu um argumento. Insira o caminho do arquivo CSV corretamente:')
+        sys.argv.insert(1, '')
+        sys.argv[1] = input()
+        while sys.argv[1] != './csv/bancoColaboradores.csv':
+            print('Argumento inválido, insira caminho do arquivo CSV corretamente.')
+            sys.argv[1] = input()
+        else:
+            if sys.argv[1] == './csv/bancoColaboradores.csv':
+                df = pd.read_csv(sys.argv[1], chunksize=5)
+                for minidf in df:
+                    print(minidf)
+                    break
 
 
 if __name__ == "__main__":
