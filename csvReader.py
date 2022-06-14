20b2b086e5e03a5196a43ff22bf913178fa067d5
harryazawa <104590702+harryazawa@users.noreply.github.com>
2022-06-14 15:19:09 -0300

    add csv and csvReader (#3)
    feat: csv e database insertion

---
+++ csvReader.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+import os
+
+
+# essa função poderá ser alterada futuramente quando for usado MySql
+def batchReading(df):
+    for minidf in df:
+        print(minidf)
+        break
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Você não inseriu um argumento. Tente novamente com o caminho de um arquivo CSV existente')
+    elif len(sys.argv) == 2:
+        arquivoCsv = sys.argv[1]
+        if not os.path.isfile(arquivoCsv):
+            print('Arquivo não encontrado, insira caminho do arquivo CSV corretamente.')
+        else:
+            df = pd.read_csv(arquivoCsv, chunksize=5)
+            batchReading(df)
+
+
+if __name__ == "__main__":
+    main()
