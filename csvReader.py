536d3a2385b59fe0429249bc94aa712f2271a55d
harryazawa <104590702+harryazawa@users.noreply.github.com>
2022-06-16 17:49:42 -0300

    feat: leitura em batch e inserção no database (#7)
    * feat: inserir/listar/buscar/informar mysql db
    
    * refactor: funções inserir/listar
    
    * fix: ajuste na leitura de batch e inserção no db

--- csvReader.py
+++ csvReader.py
@@ -1,13 +1,36 @@
 import pandas as pd
 import sys
 import os
+import bancoDefs as bd
+import validacao_colunas as vc
+import validacao_datetime as vd
+from datetime import datetime
+
+CHUNK = 5
+
+def validarCsv(dataframes):
+    vc.validarColunas(dataframes)
+    periodo_inicial = dataframes[["periodo_inicial"]]
+    periodo_final = dataframes[["periodo_final"]]
+    for i in range(periodo_inicial.size):
+        vd.validarDatas(vd.formatarData(periodo_inicial.values[i]), vd.formatarData(periodo_final.values[i]))
 
 
-# essa função poderá ser alterada futuramente quando for usado MySql
-def batchReading(df):
-    for minidf in df:
-        print(minidf)
-        break
+def conversor_df(df):
+    for i in range(0, len(df)):
+        print(df.values[i])
+        data_periodo_inicio = datetime.strptime(df.values[i][1], "%d/%m/%Y %H:%M:%S")
+        data_periodo_fim = datetime.strptime(df.values[i][2], "%d/%m/%Y %H:%M:%S")
+        print("inserting line: ", df.values[i])
+        bd.insertDataBase(df.values[i][0], data_periodo_inicio, data_periodo_fim, df.values[i][3])
+
+
+def batchReading(arquivocsv):
+    reader = pd.read_csv(arquivocsv, chunksize=CHUNK)
+    for dataframes in reader:
+        print(dataframes)
+        validarCsv(dataframes)
+        conversor_df(dataframes)
 
 
 def main():
@@ -18,8 +41,7 @@ def main():
         if not os.path.isfile(arquivoCsv):
             print('Arquivo não encontrado, insira caminho do arquivo CSV corretamente.')
         else:
-            df = pd.read_csv(arquivoCsv, chunksize=5)
-            batchReading(df)
+            batchReading(arquivoCsv)
 
 
 if __name__ == "__main__":
